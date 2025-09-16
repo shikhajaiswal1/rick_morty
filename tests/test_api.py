ab640625c254e0d5ce010c1915510d052943b635
Shikha Jaiswal <shikhajaiswal@thoughtworks.com>
2025-09-16 21:10:45 +0530

    API integration tests failing. using TestClient

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,15 @@
-import requests
+from fastapi.testclient import TestClient
+from app.main import app
 
-BASE_URL = "http://localhost:8000"
+client = TestClient(app)
 
 def test_healthcheck():
-    resp = requests.get(f"{BASE_URL}/healthcheck")
+    resp = client.get("/healthcheck")
     assert resp.status_code == 200
     assert resp.json()["status"] == "healthy"
 
 def test_characters_endpoint():
-    resp = requests.get(f"{BASE_URL}/characters?limit=5")
+    resp = client.get("/characters?limit=5")
     assert resp.status_code == 200
     data = resp.json()
     assert "results" in data
